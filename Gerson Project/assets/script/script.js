1ab8606d5d28824777146bc55e2cc1dc91c89355
Edmig1 <miguelitoplayer1@gmail.com>
2023-06-15 17:01:57 -0300

    Juros Finalizados

--- Gerson Project/assets/script/script.js
+++ Gerson Project/assets/script/script.js
@@ -1,78 +1,56 @@
 var btn = document.getElementById('form')
-btn.addEventListener('submit', function(event){
-    event.preventDefault()
-     form = new FormData(btn)
-     capital = parseFloat(form.get('capital'))
-     tempo = parseFloat(form.get('tempo'))
-     taxa =parseFloat(form.get('taxa'))
-    if( capital !== 0){
-        function valorsimples(){
-            var p = document.getElementById('p')
-            p.style.color = 'white'
-            var resultado = (capital*taxa)/100
-            var juros = resultado*tempo
-            p.innerHTML = ("O valor dos Juros a serem pagos é: "+ juros)
-            return juros
-        }
-        valorsimples()
-    }
-    if (isNaN(capital) || isNaN(tempo) || isNaN(taxa)) {
-        p.innerHTML = "Valores inválidos."
+var tipo = document.getElementById('r1')
+var valor = document.getElementById('valor')
+var tipo2 = document.getElementById('r2')
+r1.addEventListener('click', function(){
+    if(tipo.value == 'simples'){
+        var p = document.getElementById('p')
+        p.innerHTML = 'Simples'
     }
 })
-var btnjurossimples = document.getElementById('btnjs')
-btnjurossimples.addEventListener('click', function(){
-    var divform = document.getElementById('divform')
-    divform.style.display = 'flex'
-    var main = document.getElementById('main')
-    main.style.display = 'grid'
-    var option = document.getElementById('option')
-    option.style.display = 'flex'
-    option.style.flexDirection = 'column'
-    option.style.justifyContent = 'center'
-    option.style.alignItems = 'center'
-    var p = document.getElementById('p')
-    p.innerHTML = ''
-    main.style.gridTemplateColumns = '1fr 1fr'
-})
-var btnfechar = document.getElementById('btnfechar')
-btnfechar.addEventListener('click', function(){
-    var divform = document.getElementById('divform')
-    var option = document.getElementById('option')
-    option.style.display = 'flex'
-    option.style.flexDirection = 'row'
-    option.style.justifyContent = 'center'
-    option.style.alignItems = 'flex-start'
-    divform.style.display = 'none'
-    if (divform.style.display = 'none') {
-        main.style.gridTemplateColumns = "1fr"
+r2.addEventListener('click', function(){
+    if(tipo2.value == 'composto'){
+        var p = document.getElementById('p')
+        p.innerHTML = 'Composto'
     }
 })
-var btnjurossimples = document.getElementById('btnjc')
-btnjurossimples.addEventListener('click', function(){
-    var divform = document.getElementById('divform')
-    divform.style.display = 'flex'
-    var main = document.getElementById('main')
-    main.style.display = 'grid'
-    var option = document.getElementById('option')
-    option.style.display = 'flex'
-    option.style.flexDirection = 'column'
-    option.style.justifyContent = 'center'
-    option.style.alignItems = 'center'
-    var p = document.getElementById('p')
-    p.innerHTML = ''
-    main.style.gridTemplateColumns = '1fr 1fr'
-    if( capital !== 0){
-        function valorcomposto(){
-            var p = document.getElementById('p')
-            p.style.color = 'white'
-            var resultado = ((taxa/100 +1)**tempo)*capital
-            p.innerHTML = ("O valor dos Juros a serem pagos é: "+ resultado)
-            return resultado
-        }
-        valorcomposto()
-    }
-    if (isNaN(capital) || isNaN(tempo) || isNaN(taxa)) {
-        p.innerHTML = "Valores inválidos."
-    }
+    btn.addEventListener('submit', function(event){
+        event.preventDefault()
+         form = new FormData(btn)
+         capital = parseFloat(form.get('capital'))
+         tempo = parseFloat(form.get('tempo'))
+         taxa =parseFloat(form.get('taxa'))
+         if(tipo.checked == true){
+            js()
+         }
+
+    })
+    btn.addEventListener('submit', function(event){
+        event.preventDefault()
+         form = new FormData(btn)
+         capital = parseFloat(form.get('capital'))
+         tempo = parseFloat(form.get('tempo'))
+         taxa =parseFloat(form.get('taxa'))
+         if(tipo2.checked == true){
+            jc()
+         }
 })
+function js(){
+    let resultado = capital*(taxa/100)*tempo
+    console.log(resultado)
+    r = resultado.toFixed(2)
+    rc = (resultado + capital).toFixed(2)
+    valor.innerHTML = 'O Valor total a ser pago é: '+ rc + ', E o juro é: '+ r
+    return resultado
+    
+}
+function jc(){
+    let resultado = (((taxa/100)+1)**tempo)*capital
+    console.log(resultado)
+    r = resultado.toFixed(2)
+    rc = (resultado- capital).toFixed(2)
+    valor.innerHTML = 'O Valor total a ser pago é: '+ r + ', E o juro é: '+ rc
+    return resultado
+    
+}
+
